e9bcd4a5fc9b8800bd0f8265f380fc7e2cbc0d4e
Ace-CoLd-HOoDy <121887597+Ace-CoLd-HOoDy@users.noreply.github.com>
2024-12-01 14:01:10 +0000

    debugged, finished fully

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 catalog = {
-    "Bubble Sus": 1200,x
+    "Bubble Sus": 1200,
     "AmongUs": 42,
     "Capybara Clicker": 234,
     "CS:GO": 400
@@ -8,7 +8,7 @@ catalog = {
 print("Hello! This is the catalog of our games: ")
 
 for game, price in catalog.items():
-    print(f'{game} costs {price} UAH')
+    print(f'{game} costs: {price} UAH')
 
 budget = int(input("What is your budget?\n"))
 cart = []
